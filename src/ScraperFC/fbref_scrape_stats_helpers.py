d73018574061d6dc282747f90fddfac2cd5a6a23
XSlaidx <76040416+XSlaidx@users.noreply.github.com>
2026-04-06 14:44:05 +0300

    Refactor player DataFrame handling in scraping functions

--- src/ScraperFC/fbref_scrape_stats_helpers.py
+++ src/ScraperFC/fbref_scrape_stats_helpers.py
@@ -5,7 +5,7 @@ from io import StringIO
 import re
 from typing import TYPE_CHECKING
 from bs4 import Tag
-from .fbref_helpers import _get_ids_from_table, _get_stats_table_tag
+from .fbref_helpers import _get_ids_from_table, _get_stats_table_tag, _get_age_mask
 
 if TYPE_CHECKING:
     from .fbref import FBref
@@ -27,7 +27,7 @@ def _scrape_not_big5_stats(
         {"name": "table", "attrs": {"id": re.compile(f"{stats_categories[stat_category]['html']}_for")}}
     )
     if isinstance(squad_table_tag, Tag):
-        squad_df = pd.read_html(StringIO(str(squad_table_tag)))[0]
+        squad_df = pd.read_html(StringIO(str(squad_table_tag)))[0].copy()
 
         # Add team IDs for the "for" table
         squad_ids = _get_ids_from_table(squad_table_tag, "team")
@@ -42,7 +42,7 @@ def _scrape_not_big5_stats(
         {"name": "table", "attrs": {"id": re.compile(f"{stats_categories[stat_category]['html']}_against")}}
     )
     if isinstance(opp_table_tag, Tag):
-        opp_df = pd.read_html(StringIO(str(opp_table_tag)))[0]
+        opp_df = pd.read_html(StringIO(str(opp_table_tag)))[0].copy()
 
         # Add team IDs for the "against" table
         opps_ids = _get_ids_from_table(opp_table_tag, "team")
@@ -57,11 +57,13 @@ def _scrape_not_big5_stats(
         {"name": "table", "attrs": {"id": re.compile(f"stats_{stats_categories[stat_category]['html']}")}}
     )
     if isinstance(player_table_tag, Tag):
-        player_df = pd.read_html(StringIO(str(player_table_tag)))[0]
+        player_df = pd.read_html(StringIO(str(player_table_tag)))[0].copy()
 
         # Add player IDs
         player_ids = _get_ids_from_table(player_table_tag, "player")
-        player_df.loc[player_df[('Unnamed: 0_level_0', 'Rk')] != 'Rk', 'Player ID'] = player_ids
+        mask = player_df[('Unnamed: 0_level_0', 'Rk')] != 'Rk'
+        ids_aligned = pd.Series(player_ids, index=player_df.index[mask][:len(player_ids)])
+        player_df.loc[ids_aligned.index, "Player ID"] = ids_aligned
     elif player_table_tag is None:
         print(f'\nWARNING: Player stats table from {stat_url} is None.')
         player_df = pd.DataFrame()
@@ -86,7 +88,7 @@ def _scrape_big5_stats(
         {"name": "table", "attrs": {"id": re.compile(f"{stats_categories[stat_category]['html']}_for")}}
     )
     if isinstance(squad_table_tag, Tag):
-        squad_df = pd.read_html(StringIO(str(squad_table_tag)))[0]
+        squad_df = pd.read_html(StringIO(str(squad_table_tag)))[0].copy()
 
         # Add team IDs for squad stats
         squad_ids = _get_ids_from_table(squad_table_tag, "team")
@@ -101,7 +103,7 @@ def _scrape_big5_stats(
         {"name": "table", "attrs": {"id": re.compile(f"{stats_categories[stat_category]['html']}_against")}}
     )
     if isinstance(opp_table_tag, Tag):
-        opp_df = pd.read_html(StringIO(str(opp_table_tag)))[0]
+        opp_df = pd.read_html(StringIO(str(opp_table_tag)))[0].copy()
 
         # Add team IDs for opponent stats
         opp_ids = _get_ids_from_table(opp_table_tag, "team")
@@ -121,14 +123,14 @@ def _scrape_big5_stats(
 
     player_table_tag = _get_stats_table_tag(
         soup,
-        {"name": "table", "attrs": {"id": f"stats_{stats_categories[stat_category]['html']}"}}
+        {"name": "table", "attrs": {"id": re.compile(f"stats_{stats_categories[stat_category]['html']}")}}
     )
     if isinstance(player_table_tag, Tag):
-        player_df = pd.read_html(StringIO(str(player_table_tag)))[0]
+        player_df = pd.read_html(StringIO(str(player_table_tag)))[0].copy()
 
         # Add player IDs
         player_ids = _get_ids_from_table(player_table_tag, "player")
-        player_df.loc[player_df[('Unnamed: 0_level_0', 'Rk')] != 'Rk', 'Player ID'] = player_ids
+        player_df['Player ID'] = player_ids
     elif player_table_tag is None:
         print(f'\nWARNING: Player stats table from {player_stats_url} is None.')
         player_df = pd.DataFrame()
